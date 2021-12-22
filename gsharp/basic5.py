138b191505d5fde39c941a294d9023a4eeecf0a3
aarshgarg <aarshgarg0@gmail.com>
2021-12-22 21:28:55 +0530

    basic5

---
+++ gsharp/basic5.py
@@ -0,0 +1,17 @@
+strmy="Aarsh is the engineer"
+strmm="Aarshisbig"
+print(strmy)
+print(len(strmy))
+print(strmy[0:7])
+print(strmy[:])
+print(strmy[0:])
+print(strmy[:10])
+print(strmy[0:9:2])
+print(strmy[::-1])
+print(strmy.isalnum())
+print(strmm.isalnum())
+print(strmy.endswith("engineer"))
+print(strmy.count("e"))
+print(strmy.find("engineer"))
+
+
