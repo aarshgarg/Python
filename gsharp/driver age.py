c35d4e559c73935ed6bfd8dcadea75d8e340fa4e
aarshgarg <aarshgarg0@gmail.com>
2022-01-17 12:13:50 +0530

    if else 3

---
+++ gsharp/driver age.py
@@ -0,0 +1,10 @@
+print("What is your age:")
+age=int(input())
+if age<18:
+    print("You cannot drive the vechicle")
+elif age==18:
+    print("You can think about it ")
+else:
+    print("you can ride the vechicle")
+
+
