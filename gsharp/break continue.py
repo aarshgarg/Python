04a90958520ecd39c24e3c17b20240e1287960a3
aarshgarg <aarshgarg0@gmail.com>
2022-01-24 10:43:20 +0530

    break continue

---
+++ gsharp/break continue.py
@@ -0,0 +1,9 @@
+while(True):
+    print("Enter the number=",end="")
+    inp=int(input())
+    if(inp>100):
+        print("congrats")
+        break
+    else:
+        print("try again")
+        continue
