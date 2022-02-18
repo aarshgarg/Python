36c34358fe5204110ce749bb8f9a82f654cd5f1b
aarshgarg <aarshgarg0@gmail.com>
2022-02-18 12:35:21 +0530

    if else 3

--- gsharp/base2.py
+++ gsharp/base2.py
@@ -1,7 +1,21 @@
-var1="Hello world"
+"""var1="Hello world"
 var2= 5
 var3= 67.9
 print(type(var1))
 print(type(var2))
 print(type(var3))
 print(var2+var3)
+print(id(var2))
+a=b=c=22
+print(a)
+print(b)
+print(c)
+x,y,z=10,20,30
+print(x)
+print(y)
+print(z)"""
+
+str1="hello"
+str2="bye"
+print(str1+str2)
+print(str1[1:4])
