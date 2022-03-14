4fbdd676430a4f12fee213da85f6760272e2bf98
aarshgarg <aarshgarg0@gmail.com>
2022-03-14 22:30:19 +0530

    with parameters

---
+++ gsharp/Area of circle1.py
@@ -0,0 +1,7 @@
+def area(radius):
+ pi=22/7
+
+ print("The Area Of The Circle Is=",pi*radius*radius," square units")
+for i in range(1,11):
+    radius = float(input("Enter The Radius Of The Circle="))
+    i = area(radius)
