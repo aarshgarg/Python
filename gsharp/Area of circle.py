ad2afa3306ca2fad593694340112ac93f5d06f6a
aarshgarg <aarshgarg0@gmail.com>
2022-03-14 21:53:27 +0530

    Area of circle without parameters

---
+++ gsharp/Area of circle.py
@@ -0,0 +1,8 @@
+def area():
+    pi=22/7
+    radius=float(input("Enter the radius of the circle = "))
+    aoc=pi*radius*radius
+    print("Radius of the circle is =",aoc,"square units ")
+
+for i in range(1,10):
+    i=area()
