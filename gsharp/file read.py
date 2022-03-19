641ae179f0bdd29aa49a2324111e3d4eb89b6bcf
aarshgarg <aarshgarg0@gmail.com>
2022-03-20 00:35:34 +0530

    reading from txt file

---
+++ gsharp/file read.py
@@ -0,0 +1,13 @@
+f=open("aarsh.txt")
+#mat=f.read()
+#print(mat)
+
+"""mat=f.read(3)
+print(mat)
+mat=f.read(3)
+print(mat)
+mat=f.read(39)
+print(mat)"""
+
+#print(f.readlines())
+print(f.readline())
