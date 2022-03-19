a04b6f9a0e6e5381f66c834f31353213f968cff9
aarshgarg <aarshgarg0@gmail.com>
2022-03-19 22:53:40 +0530

    practice

---
+++ gsharp/practice ques.py
@@ -0,0 +1,27 @@
+example="hello"
+print(example.count('l'))
+numbers = [9, 34, 11, -4, 27]
+
+# find the maximum number
+numbers.append(55)
+print(numbers)
+print(numbers.insert(3,66))
+print(max(numbers))
+print(min(numbers))
+print(sorted(numbers))
+
+str1="Hello World"
+print("string1",str1)
+str1=str1*4
+print(str1,"\t")
+
+str2="xyyzxyzxzxyy"
+print(str2.count('yy'))
+
+list1=[9, 34, 11, 4, 27]
+list1.reverse()
+print(list1)
+
+str4="Hello world"
+print(str4.replace('w','s'))
+
